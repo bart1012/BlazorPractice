4bb2055ec621da18ea43d653a4b09e876c2be166
stripi <cgm998cgm@gmail.com>
2025-01-21 11:01:36 +0000

    Implement homepage

---
+++ BlazorApp1/BlazorApp1/Models/Compliment.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Compliment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Models/Tree.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Tree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public (double x, double y) Location { get; set; }
+        public double Size { get; set; }
+        public string Species { get; set; }
+        public List<string> Compliments { get; set; }
+
+    }
+}
